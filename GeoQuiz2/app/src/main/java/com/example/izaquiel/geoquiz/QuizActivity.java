b1918a0c53dc7f847f824fb9623f5d789b452683
izaquiel7 <lotygun7@gmail.com>
2018-05-04 14:30:28 -0300

    resolvido bug de rotação

--- GeoQuiz2/app/src/main/java/com/example/izaquiel/geoquiz/QuizActivity.java
+++ GeoQuiz2/app/src/main/java/com/example/izaquiel/geoquiz/QuizActivity.java
@@ -2,12 +2,17 @@ package com.example.izaquiel.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private static final String tag = "QuizActivity";
+    private static final String i_chave = "index";
+
     private Button bTrue;
     private Button bFalse;
     private Button bNext;
@@ -21,13 +26,24 @@ public class QuizActivity extends AppCompatActivity {
             new Question(R.string.question2, true),
             new Question(R.string.question3, false),
             new Question(R.string.question4, true),
-            new Question(R.string.question5, true)
+            new Question(R.string.question5, true),
+            new Question(R.string.question6, true),
+            new Question(R.string.question7, true),
+            new Question(R.string.question8, true),
+            new Question(R.string.question9, true),
+            new Question(R.string.question10, false)
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(tag, "Metodo o create() chamado");
         setContentView(R.layout.activity_quiz);
+
+
+        if(savedInstanceState !=null) {
+            currentIndex = savedInstanceState.getInt(i_chave, 0);
+        }
 
         bBack = (Button) findViewById(R.id.backbuttonId);
         bTrue = (Button) findViewById(R.id.Btrue);
@@ -92,5 +108,45 @@ public class QuizActivity extends AppCompatActivity {
     private void updateQuestion(){
         int question = questionBank[currentIndex] .getIdTextRes();
         questionTextView.setText(question);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        Log.d(tag, "esse negocio chamou onStart()");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        Log.d(tag, "esse negocio chamou onResume()");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        Log.d(tag, "esse negocio chamou onPause()");
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+
+        Log.i(tag, "salvando a instancia do estado.");
+        savedInstanceState.putInt(i_chave, currentIndex);
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+
+        Log.d(tag, "esse negocio chamou onStop()");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+
+        Log.d(tag, "esse negocio chamou onDestroy()");
     }
 }
